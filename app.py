c31a04759c1a9de98b1963ee4afc9ea719b513c3
Shadowfa-cmd <babaqomastan29@gmail.com>
2026-02-18 22:46:17 +0430

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@app.route('/')
+def home():
+    return "Chat Server is Running!"
+
+@socketio.on('message')
+def handle_message(msg):
+    print("Message: " + msg)
+    send(msg, broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app, host='0.0.0.0', port=10000)
